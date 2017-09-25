df0e664af2e94306985f06178a4fdb28d39cf2df
Collin Walther <collinwalther@hotmail.com>
2017-09-25 13:23:03 -0700

    Added feature that updates the number of results on every search

--- project.php
+++ project.php
@@ -10,14 +10,14 @@
 
     # Output the amount of results.
     if($results->fetch_assoc() == NULL or $results->num_rows == 0) {
-      echo '<h1>No results.</h1>';
+      echo '<h1 id="numResults">No results.</h1>';
       return;
     }
     elseif ($results->num_rows == 1) {
-      echo '<h1>1 result:</h1>';
+      echo '<h1 id="numResults">1 result:</h1>';
     }
     else {
-      echo '<h1>' . $results->num_rows . ' results:</h1>';
+      echo '<h1 id="numResults">' . $results->num_rows . ' results:</h1>';
     }
     echo '<table class="table table-striped table-responsive"><thead><tr>';
 
@@ -78,11 +78,13 @@
 	<div class="container-fluid">
       <div class="row">
         <div class="col-md-3">
+          <div class = "form-group">
           <div class="well">
             <h1>Queries:</h1>
-            <p>Other school's name: <input type="text" id="otherSchoolName" placeholder="School name"></p>
-            <p>Other school's course code: <input type="text" id="otherCourseCode" placeholder="Course code"></p>
-            <p>SCU's course code: <input type="text" id="localCourseCode" placeholder="Course code"></p>
+            <p>Other school's name: <input type="text" id="otherSchoolName" class="form-control" placeholder="School name"></p>
+            <p>Other school's course code: <input type="text" id="otherCourseCode" class="form-control" placeholder="Course code"></p>
+            <p>SCU's course code: <input type="text" id="localCourseCode" class="form-control" placeholder="Course code"></p>
+          </div>
           </div>
         </div>
       <div class="col-md-9">
@@ -137,6 +139,17 @@
       var localCourseCodeMatches = !~localCourseCode.indexOf(localCourseCodesSearch);
       return otherSchoolNameMatches || otherCourseCodeMatches || localCourseCodeMatches;
     }).hide();
+
+    // Update the number of results of the search.
+    var numOfVisibleRows = $('#results tr:visible').length;
+    console.log("numResults: " + numOfVisibleRows);
+    if (numOfVisibleRows < 1) {
+        $('#numResults').html("No results");
+    } else if (numOfVisibleRows == 1) {
+        $('#numResults').html("1 result");
+    } else {
+        $('#numResults').html(numOfVisibleRows + " results");
+    }
   }
 
   // Any time one of the search terms is changed, update the search results.
